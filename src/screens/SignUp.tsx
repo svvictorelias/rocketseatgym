746676ec1e6c7f050184356e43a87aad5d4925ba
Victor Elias <svvictorelias@gmail.com>
2022-12-20 15:34:20 -0300

    feat: first commit

---
+++ src/screens/SignUp.tsx
@@ -0,0 +1,56 @@
+import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import LogoSvg from "@assets/logo.svg";
+import BackgroundImg from "@assets/background.png";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
+import { useNavigation } from "@react-navigation/native";
+export function SignUp() {
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} px={10} pb={16}>
+        <Image
+          source={BackgroundImg}
+          defaultSource={BackgroundImg}
+          alt="Pessoas treinando"
+          resizeMode="contain"
+          position="absolute"
+        />
+        <Center my={24}>
+          <LogoSvg />
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
+        <Center>
+          <Heading color="gray.100" mb={6} fontSize="xl" fontFamily="heading">
+            Crie sua conta
+          </Heading>
+          <Input placeholder="Nome" />
+          <Input
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <Input placeholder="Senha" secureTextEntry />
+          <Button title="Criar e acessar" />
+        </Center>
+
+        <Button
+          mt={12}
+          title="Voltar para o login"
+          variant="outline"
+          onPress={handleGoBack}
+        />
+      </VStack>
+    </ScrollView>
+  );
+}
